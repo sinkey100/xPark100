5dc725c168dfbf2ad64e2ad92c93baa0bdbc7059
Sinkey100 <36932551@qq.com>
2024-08-07 10:33:33 +0800

    feat: 拉取数据增加log展示

--- app/command/Xpark.php
+++ app/command/Xpark.php
@@ -23,6 +23,7 @@ class Xpark extends Base
 
     protected function execute(Input $input, Output $output)
     {
+        $output->writeln(date("Y-m-d H:i:s") . ' 任务开始');
         // 清除老数据
         for ($i = 0; $i < 3; $i++) {
 
@@ -32,6 +33,7 @@ class Xpark extends Base
             $maxId++;
             Db::execute("ALTER TABLE `ba_xpark_data` AUTO_INCREMENT={$maxId};");
         }
+
 
         $result = $this->http('POST', 'https://manage.xpark365.com/backend/gmf-manage/report/get_report', [
             'json'    => [
@@ -44,9 +46,17 @@ class Xpark extends Base
             ]
         ]);
 
-        if (!isset($result['data']['list']) || count($result['data']['list']) == 0) {
+        if(!isset($result['data']['list'])){
+            $output->writeln(date("Y-m-d H:i:s") . ' 拉取数据完成，没有返回数据');
+            $output->writeln(json_encode($result));
             return;
         }
+        if(count($result['data']['list']) == 0){
+            $output->writeln(date("Y-m-d H:i:s") . ' 拉取数据完成，长度0');
+            $output->writeln(json_encode($result));
+            return;
+        }
+
         $data = [];
         foreach ($result['data']['list'] as $item_day) {
             $csvRaw = file_get_contents($item_day['url']);
@@ -58,7 +68,7 @@ class Xpark extends Base
 
             $data = array_merge($data, $csvData);
         }
-
+        $output->writeln(date("Y-m-d H:i:s") . ' 拉取数据完成，长度' . count($data));
         Data::insertAll($data);
     }
 
