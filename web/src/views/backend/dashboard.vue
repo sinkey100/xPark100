dc4d4c09d176942cfee9cb0d4aa73ef3be07b7d3
Sinkey100 <36932551@qq.com>
2024-08-28 11:36:36 +0800

    feat: 首页ui优化

--- web/src/views/backend/dashboard.vue
+++ web/src/views/backend/dashboard.vue
@@ -1,239 +1,29 @@
 <template>
     <div class="default-main">
-        <div class="banner">
-            <div class="welcome suspension">
-                <div class="welcome-text">
-                    <div class="welcome-title">{{ adminInfo.nickname + t('utils.comma') + getGreet() }}</div>
-                </div>
-            </div>
+        <div class="tips">
+            {{ adminInfo.nickname }}, Welcome back! 🎉
         </div>
 
     </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted, onUnmounted, reactive, nextTick, onActivated, watch, onBeforeMount } from 'vue'
-import headerSvg from '/@/assets/dashboard/header-1.svg'
-import coffeeSvg from '/@/assets/dashboard/coffee.svg'
-import { CountUp } from 'countup.js'
-import * as echarts from 'echarts'
-import { useNavTabs } from '/@/stores/navTabs'
-import { useTemplateRefsList } from '@vueuse/core'
-import { index } from '/@/api/backend/dashboard'
-import { useI18n } from 'vue-i18n'
-import { Local } from '/@/utils/storage'
 import { useAdminInfo } from '/@/stores/adminInfo'
-import { WORKING_TIME } from '/@/stores/constant/cacheKey'
-import { fullUrl, getGreet } from '/@/utils/common'
-import { useEventListener } from '@vueuse/core'
-let workTimer: number
 
 defineOptions({
     name: 'dashboard',
 })
-
-const d = new Date()
-const { t } = useI18n()
-const navTabs = useNavTabs()
 const adminInfo = useAdminInfo()
-const chartRefs = useTemplateRefsList<HTMLDivElement>()
-
-
 </script>
 
 <style scoped lang="scss">
-.welcome {
-    background: #e1eaf9;
-    border-radius: 6px;
+.tips{
+    font-size:24px;
+    text-align: center;
+    color:#444;
     display: flex;
     align-items: center;
-    padding: 15px 20px !important;
-    box-shadow: 0 0 30px 0 rgba(82, 63, 105, 0.05);
-    .welcome-img {
-        height: 100px;
-        margin-right: 10px;
-        user-select: none;
-    }
-    .welcome-title {
-        font-size: 1.5rem;
-        line-height: 30px;
-        color: var(--ba-color-primary-light);
-    }
-    .welcome-note {
-        padding-top: 6px;
-        font-size: 15px;
-        color: var(--el-text-color-primary);
-    }
-}
-.working {
-    height: 130px;
-    display: flex;
     justify-content: center;
-    flex-wrap: wrap;
-    height: 100%;
-    position: relative;
-    &:hover {
-        .working-coffee {
-            -webkit-transform: translateY(-4px) scale(1.02);
-            -moz-transform: translateY(-4px) scale(1.02);
-            -ms-transform: translateY(-4px) scale(1.02);
-            -o-transform: translateY(-4px) scale(1.02);
-            transform: translateY(-4px) scale(1.02);
-            z-index: 999;
-        }
-    }
-    .working-coffee {
-        transition: all 0.3s ease;
-        width: 80px;
-    }
-    .working-text {
-        display: block;
-        width: 100%;
-        font-size: 15px;
-        text-align: center;
-        color: var(--el-text-color-primary);
-    }
-    .working-opt {
-        position: absolute;
-        top: -40px;
-        right: 10px;
-        background-color: rgba($color: #000000, $alpha: 0.3);
-        padding: 10px 20px;
-        border-radius: 20px;
-        color: var(--ba-bg-color-overlay);
-        transition: all 0.3s ease;
-        cursor: pointer;
-        opacity: 0;
-        z-index: 999;
-        &:active {
-            background-color: rgba($color: #000000, $alpha: 0.6);
-        }
-    }
-    &:hover {
-        .working-opt {
-            opacity: 1;
-            top: 0;
-        }
-        .working-done {
-            opacity: 1;
-            top: 50px;
-        }
-    }
-}
-.small-panel-box {
-    margin-top: 20px;
-}
-.small-panel {
-    background-color: #e9edf2;
-    border-radius: var(--el-border-radius-base);
-    padding: 25px;
-    margin-bottom: 20px;
-    .small-panel-title {
-        color: #92969a;
-        font-size: 15px;
-    }
-    .small-panel-content {
-        display: flex;
-        align-items: flex-end;
-        margin-top: 20px;
-        color: #2c3f5d;
-        .content-left {
-            font-size: 24px;
-            .icon {
-                margin-right: 10px;
-            }
-            span {
-                display: inline-block;
-                font-size: 28px;
-            }
-        }
-        .content-right {
-            font-size: 18px;
-            margin-left: auto;
-        }
-        .color-success {
-            color: var(--el-color-success);
-        }
-        .color-warning {
-            color: var(--el-color-warning);
-        }
-        .color-danger {
-            color: var(--el-color-danger);
-        }
-        .color-info {
-            color: var(--el-text-color-secondary);
-        }
-    }
-}
-.growth-chart {
-    margin-bottom: 20px;
-}
-.user-growth-chart,
-.file-growth-chart {
-    height: 260px;
-}
-.new-user-growth {
-    height: 300px;
-}
-
-.user-source-chart,
-.user-surname-chart {
-    height: 400px;
-}
-.new-user-item {
-    display: flex;
-    align-items: center;
-    padding: 20px;
-    margin: 10px 15px;
-    box-shadow: 0 0 30px 0 rgba(82, 63, 105, 0.05);
-    background-color: var(--ba-bg-color-overlay);
-    .new-user-avatar {
-        height: 48px;
-        width: 48px;
-        border-radius: 50%;
-    }
-    .new-user-base {
-        margin-left: 10px;
-        color: #2c3f5d;
-        .new-user-name {
-            font-size: 15px;
-        }
-        .new-user-time {
-            font-size: 13px;
-        }
-    }
-    .new-user-arrow {
-        margin-left: auto;
-    }
-}
-.new-user-card :deep(.el-card__body) {
-    padding: 0;
-}
-
-@media screen and (max-width: 425px) {
-    .welcome-img {
-        display: none;
-    }
-}
-@media screen and (max-width: 1200px) {
-    .lg-mb-20 {
-        margin-bottom: 20px;
-    }
-}
-html.dark {
-    .welcome {
-        background-color: var(--ba-bg-color-overlay);
-    }
-    .small-panel {
-        background-color: var(--ba-bg-color-overlay);
-        .small-panel-content {
-            color: var(--el-text-color-regular);
-        }
-    }
-    .new-user-item {
-        .new-user-base {
-            color: var(--el-text-color-regular);
-        }
-    }
+    height: 70vh;
 }
 </style>
