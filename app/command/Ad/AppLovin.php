4c18d26ba155152fe12e7b3d1139229b27ee1f37
Sinkey100 <36932551@qq.com>
2025-01-20 18:56:17 +0800

    feat: AppLovin支持多账号

--- app/command/Ad/AppLovin.php
+++ app/command/Ad/AppLovin.php
@@ -2,6 +2,7 @@
 
 namespace app\command\Ad;
 
+use app\admin\model\xpark\Channel;
 use app\admin\model\xpark\Data;
 use app\admin\model\xpark\Domain;
 use app\command\Base;
@@ -63,64 +64,67 @@ class AppLovin extends Base
         $ad_domains = Domain::where('channel', 'AppLovin')->select()->toArray();
         $ad_domains = array_column($ad_domains, 'domain');
         $days       = $this->days - 1;
+        // 获取通道数量
+        $applovin_channels = Channel::where('channel_type', 'AppLovin')->select()->toArray();
 
-        $result = $this->http('GET', 'https://r.applovin.com/maxReport', [
-            'query' => [
-                'api_key' => Env::get('APPLOVIN.REPORT_KEY'),
-                'columns' => implode(",", [
-                    'attempts', 'package_name', 'day', 'country', 'max_ad_unit_id',
-                    'responses', 'impressions', 'estimated_revenue', 'ecpm', 'network'
-                ]),
-                "start"   => date("Y-m-d", strtotime("-$days days")),
-                "end"     => date("Y-m-d"),
-                'format'  => 'json'
-            ]
-        ]);
+        foreach ($applovin_channels as $applovin_channel){
+            $result = $this->http('GET', 'https://r.applovin.com/maxReport', [
+                'query' => [
+                    'api_key' => Env::get('APPLOVIN.REPORT_KEY_' . $applovin_channel['id']),
+                    'columns' => implode(",", [
+                        'attempts', 'package_name', 'day', 'country', 'max_ad_unit_id',
+                        'responses', 'impressions', 'estimated_revenue', 'ecpm', 'network'
+                    ]),
+                    "start"   => date("Y-m-d", strtotime("-$days days")),
+                    "end"     => date("Y-m-d"),
+                    'format'  => 'json'
+                ]
+            ]);
 
-        if (!(isset($result['code']) && $result['code'] == 200)) {
-            $this->log('拉取数据错误');
-            $this->log(json_encode($result));
-            return;
+            if (!(isset($result['code']) && $result['code'] == 200)) {
+                $this->log('拉取数据错误');
+                $this->log(json_encode($result));
+                return;
+            }
+
+
+            if (count($result['results']) == 0) {
+                $this->log('拉取数据完成，长度0');
+                $this->log(json_encode($result));
+                return;
+            }
+            $saveData = [];
+            foreach ($result['results'] as $v) {
+                if (!in_array($v['package_name'], $ad_domains)) continue;
+
+                [$domain_id, $app_id] = $this->getDomainRow($v['package_name'], $v['day'], 'AppLovin');
+
+                $saveData[] = [
+                    'channel'         => 'AppLovin',
+                    'channel_full'    => $applovin_channel['channel_alias'],
+                    'channel_id'      => $applovin_channel['id'],
+                    'channel_type'    => 1,
+                    'sub_channel'     => $v['package_name'],
+                    'domain_id'       => $domain_id,
+                    'app_id'          => $app_id,
+                    'a_date'          => $v['day'],
+                    'country_code'    => strtoupper($v['country']),
+                    'ad_placement_id' => $v['max_ad_unit_id'],
+                    'requests'        => $v['attempts'],
+                    'fills'           => $v['responses'],
+                    'impressions'     => $v['impressions'],
+                    'clicks'          => 0,
+                    'ad_revenue'      => $v['estimated_revenue'],
+                    'gross_revenue'   => $v['estimated_revenue'],
+                    'net_revenue'     => $v['estimated_revenue'],
+                    'raw_cpc'         => 0,
+                    'raw_ctr'         => 0,
+                    'raw_ecpm'        => $v['ecpm']
+                ];
+            }
+
+            $this->saveData($saveData);
         }
-
-
-        if (count($result['results']) == 0) {
-            $this->log('拉取数据完成，长度0');
-            $this->log(json_encode($result));
-            return;
-        }
-        $saveData = [];
-        foreach ($result['results'] as $v) {
-            if (!in_array($v['package_name'], $ad_domains)) continue;
-
-            [$domain_id, $app_id] = $this->getDomainRow($v['package_name'], $v['day'], 'AppLovin');
-            $channel_full = 'AppLovin';
-
-            $saveData[] = [
-                'channel'         => 'AppLovin',
-                'channel_full'    => $channel_full,
-                'channel_id'      => $this->channelList[$channel_full]['id'] ?? 0,
-                'channel_type'    => ($this->channelList[$channel_full]['ad_type'] ?? 'H5') == 'H5' ? 0 : 1,
-                'sub_channel'     => $v['package_name'],
-                'domain_id'       => $domain_id,
-                'app_id'          => $app_id,
-                'a_date'          => $v['day'],
-                'country_code'    => strtoupper($v['country']),
-                'ad_placement_id' => $v['max_ad_unit_id'],
-                'requests'        => $v['attempts'],
-                'fills'           => $v['responses'],
-                'impressions'     => $v['impressions'],
-                'clicks'          => 0,
-                'ad_revenue'      => $v['estimated_revenue'],
-                'gross_revenue'   => $v['estimated_revenue'],
-                'net_revenue'     => $v['estimated_revenue'],
-                'raw_cpc'         => 0,
-                'raw_ctr'         => 0,
-                'raw_ecpm'        => $v['ecpm']
-            ];
-        }
-
-        $this->saveData($saveData);
 
     }
 
